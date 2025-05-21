afaf6ffb669f98dcc90d798698135f91d34eca8c
EpicurianCoder <callum@callumpickard.com>
2025-05-21 20:29:56 +0200

    fix import

--- src/pages/api/pull.js
+++ src/pages/api/pull.js
@@ -1,4 +1,4 @@
-import createClient from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/server";
 
 // get request that makes a call to the supabase database
 // to get the items for the user
